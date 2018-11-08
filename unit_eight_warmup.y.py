104eec0f2d5ab94a76198c749b8617d70df11fc5
Lliam <lliam@iMac8.local>
2018-11-08 12:20:57 -0500

    Add variables

--- unit_eight_warmup.y.py
+++ unit_eight_warmup.y.py
@@ -3,15 +3,19 @@ import tkinter
 root = tkinter.Tk()
 root.title("Temperature Converter")
 
+f_value = tkinter.StringVar()
+c_value = tkinter.StringVar()
+
 f_label = tkinter.Label(root, text="Degrees Fahrenheit")
 f_label.grid(row=1, column=1)
 
-f_label_entry = tkinter.Entry(root)
+f_label_entry = tkinter.Entry(root, textvariable=f_value)
 f_label_entry.grid(row=1, column=2)
 
 C_label = tkinter.Label(root, text="Degrees Celsius")
 C_label.grid(row=2, column=1)
 
-C_label_entry = tkinter.Entry(root)
+C_label_entry = tkinter.Label(root, textvariable=c_value)
 C_label_entry.grid(row=2, column=2)
+
 root.mainloop()
